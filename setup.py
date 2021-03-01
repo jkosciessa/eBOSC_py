ac5d410b7735f4315a1ac07bfc0618afeda1f4b4
Julian Kosciessa <kosciessa@mpib-berlin.mpg.de>
2021-03-01 11:11:27 +0100

    clean up setup

--- setup.py
+++ setup.py
@@ -2,30 +2,26 @@ from setuptools import setup, find_packages
 
 classifiers = [
     # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering',
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
+    'Operating System :: Unix',
+    'Operating System :: POSIX',
+    'Operating System :: Microsoft :: Windows',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering',
 ]
 
 setup(
     name='eBOSC-py',
-    version='0.0.1',
+    version='0.95-dev',
     license='LGPL-3.0-or-later',
     description='extended Better Oscillation Detection, implemented in python3',
     maintainer = 'Julian Kosciessa',
     maintainer_email = 'kosciessa@mpib-berlin.mpg.de',
     url='https://github.com/jkosciessa/eBOSC_py/',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    include_package_data=True,
-    zip_safe=False,
+    packages=find_packages(),
     classifiers=classifiers,
     project_urls={
         'Issue Tracker': 'https://github.com/jkosciessa/eBOSC_py/issues',
