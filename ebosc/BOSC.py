fix a few off-by-one errors

ebosc/BOSC.py
@@ -36,12 +36,12 @@ def BOSC_tf(eegsignal,F,Fsample,wavenumber):
     # loop through sampled frequencies
     for f in range(len(F)):
         #print(f)
-        t=np.arange(-3.6*st[f],(3.6*st[f])+1/Fsample,1/Fsample)
+        t=np.arange(-3.6*st[f],(3.6*st[f]),1/Fsample)
         # define Morlet wavelet
         m=A[f]*np.exp(-t**2/(2*st[f]**2))*np.exp(1j*2*np.pi*F[f]*t)
         y=np.convolve(eegsignal,m, 'full')
         y=abs(y)**2
-        B[f,:]=y[np.arange(int(np.ceil(len(m)/2)), len(y)-int(np.floor(len(m)/2)) + 1, 1)]
+        B[f,:]=y[np.arange(int(np.ceil(len(m)/2))-1, len(y)-int(np.floor(len(m)/2)), 1)]
         T=np.arange(1,len(eegsignal)+1,1)/Fsample
     return B, T, F
 
@@ -96,12 +96,12 @@ def BOSC_detect(b,powthresh,durthresh,Fsample):
     if not any(pos) and not any(neg):
         # either all time points are rhythmic or none
         if all(x==1):
-            H = np.array([[1],[nT]])
+            H = np.array([[0],[nT]])
         elif all(x==0):
             H = np.array([])
     elif not any(pos):
         # i.e., starts on an episode, then stops
-        H = np.array([[1],neg])
+        H = np.array([[0],neg])
         #np.concatenate(([1],neg), axis=0)
     elif not any(neg):
         # starts, then ends on an ep.
@@ -111,7 +111,7 @@ def BOSC_detect(b,powthresh,durthresh,Fsample):
         # special-case, create the H double-vector
         if pos[0]>neg[0]:
             # we start with an episode
-            pos = np.append(1,pos)
+            pos = np.append(0,pos)
         if neg[-1]<pos[-1]:
             # we end with an episode
             neg = np.append(neg,nT)

ebosc/eBOSC.py
@@ -77,9 +77,10 @@ def eBOSC_getThresholds(cfg_eBOSC, TFR, eBOSC):
             # find empirical peak in specified range
             freqInd1 = np.where(cfg_eBOSC['F'] >= cfg_eBOSC['threshold.excludePeak'][indExFreq,0])[0][0]
             freqInd2 = np.where(cfg_eBOSC['F'] <= cfg_eBOSC['threshold.excludePeak'][indExFreq,1])[-1][-1]
-            meanbg_within_range = list(BG[freqInd1:freqInd2,:].mean(1))
+            freqidx = np.arange(freqInd1,freqInd2+1)
+            meanbg_within_range = list(BG[freqidx,:].mean(1))
             indPos = meanbg_within_range.index(max(meanbg_within_range))
-            indPos = freqInd1+indPos
+            indPos = freqidx[indPos]
             # approximate wavelet extension in frequency domain
             # note: we do not remove the specified range, but the FWHM
             # around the empirical peak
@@ -143,19 +144,19 @@ def eBOSC_episode_sparsefreq(cfg_eBOSC, detected, TFR):
     for [indF,valF] in enumerate(cfg_eBOSC['F']):
         #print(indF)
         lastVal = np.where(cfg_eBOSC['F']<=lowFreq[indF])[0]
-        if not not any(lastVal):
+        if len(lastVal)>0:
             # first freq falling into range
             fmat[indF,0] = lastVal[-1]+1
-        else: fmat[indF,0] = 1
+        else: fmat[indF,0] = 0
         firstVal = np.where(cfg_eBOSC['F']>=highFreq[indF])[0]
-        if not not any(firstVal):
+        if len(firstVal)>0:
             # last freq falling into range
             fmat[indF,2] = firstVal[0]-1
-        else: fmat[indF,2] = cfg_eBOSC['F'].shape[0]
-    fmat[:,1] = np.arange(1, cfg_eBOSC['F'].shape[0]+1,1)
+        else: fmat[indF,2] = cfg_eBOSC['F'].shape[0]-1
+    fmat[:,1] = np.arange(0, cfg_eBOSC['F'].shape[0],1)
     del indF
     range_cur = np.diff(fmat, axis=1)
-    range_cur = np.max(range_cur,axis=0)
+    range_cur = [int(np.max(range_cur[:,0])), int(np.max(range_cur[:,1]))]
     # %% perform the actual search
     # initialize variables
     # append frequency search space (i.e. range at both ends. first index refers to lower range
@@ -373,7 +374,7 @@ def eBOSC_episode_postproc_maxbias(cfg_eBOSC, episodes, TFR):
         [tmp_bias_mat, tmp_time, tmp_freq] = BOSC_tf(signal,cfg_eBOSC['F'],cfg_eBOSC['fsample'],cfg_eBOSC['wavenumber'])
         # bias matrix
         points_begin = np.arange(0,N_tp+1)
-        points_end = np.arange(N_tp,B_bias.shape[2])
+        points_end = np.arange(N_tp,B_bias.shape[2]+1)
         # for some reason, we have to transpose the matrix here, as the submatrix dimension order changes???
         B_bias[f,:,points_begin] = np.transpose(tmp_bias_mat[:,points_begin])
         B_bias[f,:,points_end] = np.transpose(np.fliplr(tmp_bias_mat[:,points_begin]))
@@ -582,7 +583,7 @@ def eBOSC_episode_create(cfg_eBOSC,TFR,detected,eBOSC):
     # formula and apply half of the BP repsonse on top of the center frequency.
     # Because of log-scaling, the widths are not the same on both sides.
     
-    detected = eBOSC_episode_sparsefreq(cfg_eBOSC, detected, TFR)
+    detected = eBOSC_episode_sparsefreq(cfg_eBOSC, detected, TFR)    
     
     # %%  Create continuous rhythmic episodes
     
@@ -608,7 +609,6 @@ def eBOSC_episode_create(cfg_eBOSC,TFR,detected,eBOSC):
         # find seed (remember that numpy uses row-first format!)
         # we need increasing x-axis sorting here
         [tmp_y,tmp_x] = np.where(np.matrix.transpose(detected_remaining)==1)
-        #[tmp_x,tmp_y] = np.where(detected_remaining==1)
         x.append(tmp_x[0])
         y.append(tmp_y[0])
         # check next sampling point
@@ -619,7 +619,7 @@ def eBOSC_episode_create(cfg_eBOSC,TFR,detected,eBOSC):
             next_freqs = np.arange(x[-1]-cfg_eBOSC['fstp'],
                           x[-1]+cfg_eBOSC['fstp']+1)
             tmp = np.where(detected_remaining[next_freqs,next_point]==1)[0]
-            if not tmp.size == 0:
+            if tmp.size > 0:
                 y.append(next_point)
                 if tmp.size > 1:
                     # JQK 161017: It is possible that an episode is branching 
@@ -627,7 +627,7 @@ def eBOSC_episode_create(cfg_eBOSC,TFR,detected,eBOSC):
                     # Note that there is no correction for 1/f here, but 
                     # practically, it leads to satisfying results 
                     # (i.e. following the longer episodes).
-                    tmp_data = tmp_B1[np.arange(x[-1]-cfg_eBOSC['fstp'],x[-1]+cfg_eBOSC['fstp']),next_point]
+                    tmp_data = tmp_B1[next_freqs,next_point]
                     tmp = np.where(tmp_data == max(tmp_data))[0]
                 x.append(next_freqs[tmp[0]])
             else:
@@ -643,8 +643,7 @@ def eBOSC_episode_create(cfg_eBOSC,TFR,detected,eBOSC):
         if len(y) >= num_pnt:
             # %% encode episode that crosses duration threshold
             episodesTable['RowID'].append(np.array(x)-cfg_eBOSC['fstp'])
-            episodesTable['ColID'].append([np.single(y[0]-cfg_eBOSC['fstp']), 
-                                    np.single(y[-1]-cfg_eBOSC['fstp'])])
+            episodesTable['ColID'].append([np.single(y[0]), np.single(y[-1])])
             episodesTable['Frequency'].append(np.single(cfg_eBOSC['F'][episodesTable['RowID'][-1]]))
             episodesTable['FrequencyMean'].append(np.single(avg_frq))
             tmp_x = episodesTable['RowID'][-1]

ebosc/eBOSC_wrapper.py
@@ -72,15 +72,17 @@ def eBOSC_wrapper(cfg_eBOSC, data):
     n_trial = len(cfg_eBOSC['trial'])
     n_freq = len(cfg_eBOSC['F'])
     n_time_total = len(pd.unique(data.loc[data['epoch']==0, ('time')]))
-    # create a new time vector that is non-continuous and starts at zero
-    cfg_eBOSC['time.time_total'] = np.arange(0, 1/cfg_eBOSC['fsample']*(n_time_total) , 1/cfg_eBOSC['fsample'])
+    # copy potentially non-continuous time values (assume that epoch is labeled 0)
+    cfg_eBOSC['time.time_total'] = data.loc[data['epoch']==0, ('time')].values
+    # alternatively: create a new time vector that is non-continuous and starts at zero
+    # np.arange(0, 1/cfg_eBOSC['fsample']*(n_time_total) , 1/cfg_eBOSC['fsample'])
     # get timing and info for post-TFR padding removal
     tfr_time2extract = np.arange(cfg_eBOSC['pad.tfr_sample']+1, n_time_total-cfg_eBOSC['pad.tfr_sample']+1,1)
     cfg_eBOSC['time.time_tfr'] = cfg_eBOSC['time.time_total'][tfr_time2extract]
     n_time_tfr = len(cfg_eBOSC['time.time_tfr'])
     # get timing and info for post-detected padding removal
     det_time2extract = np.arange(cfg_eBOSC['pad.detection_sample']+1, n_time_tfr-cfg_eBOSC['pad.detection_sample']+1,1)
-    cfg_eBOSC['time.time_det'] = cfg_eBOSC['time.time_total'][det_time2extract]
+    cfg_eBOSC['time.time_det'] = cfg_eBOSC['time.time_tfr'][det_time2extract]
     n_time_det = len(cfg_eBOSC['time.time_det'])
         
     # %% preallocate data frames

examples/eBOSC_example_empirical.py
@@ -21,7 +21,7 @@ pn['outfile'] = os.path.join(pn['root'],'data','example_out.npy')
 # %% eBOSC parameters
 
 cfg_eBOSC = dict()
-cfg_eBOSC['F'] = 2 ** np.arange(1,6,.125)   # frequency sampling
+cfg_eBOSC['F'] = 2 ** np.arange(1,6+.125,.125)   # frequency sampling
 cfg_eBOSC['wavenumber'] = 6                 # wavelet parameter (time-frequency tradeoff)
 cfg_eBOSC['fsample'] = 500                  # current sampling frequency of EEG data
 cfg_eBOSC['pad.tfr_s'] = 1                  # padding following wavelet transform to avoid edge artifacts in seconds (bi-lateral)
@@ -160,11 +160,11 @@ alphaDetected[alphaDetected==0] = np.nan
 for indEp in idx_alpha[0]:
     # These are two alternative ways to extract the onset timepoint from the table
     idx_onsetTime.append(np.where(cfg_eBOSC['time.time_det']>= eBOSC['episodes']['Onset'][indEp])[0][0])
-    idx_onset.append(eBOSC['episodes']['ColID'][indEp][0])
+    idx_onset.append(np.int_(eBOSC['episodes']['ColID'][indEp][0]))
     # Mark all periods with episodes falling into the alpha range
     within_range = np.arange(eBOSC['episodes']['ColID'][indEp][0], 
                              eBOSC['episodes']['ColID'][indEp][-1]+1)
-    alphaDetected[within_range] = 1
+    alphaDetected[np.int_(within_range)] = 1
 
 fig, ax = plt.subplots(nrows=1, ncols=1,figsize=(10,5))
 highlight_limit = max(origData)*2
@@ -180,7 +180,7 @@ rhythm.set_label('Rhythmic signal')
 plt.xlabel('Time (s)')
 plt.ylabel('Power [µV]')
 ax.legend()
-ax.set_xlim([4, 12])
+ax.set_xlim([162, 167])
 plt.show()
 
 # =============================================================================